a921501cd7a9e229abd94337d19697a798e7f864
Andrii Mashtaler <andrii.mashtaler@gmx.at>
2024-12-06 10:05:02 +0100

    Descriptions finish

---
+++ src/components/Description/Description.jsx
@@ -0,0 +1,14 @@
+import css from "./Description.module.css";
+const Title = () => {
+  return (
+    <div className={css.TitleContainer}>
+      <h1 className={css.Title}>Sip Happens Cafe</h1>
+      <p className={css.TitleSub}>
+        Please leave your feedback about our service by selecting one of the
+        options below
+      </p>
+    </div>
+  );
+};
+
+export default Title;
